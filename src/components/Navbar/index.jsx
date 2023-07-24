c7522d04f07110dd49b67505f4aa5308900423cb
Mateo <mateobetancurb@gmail.com>
2023-07-24 11:53:01 -0500

    styles and routing

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,100 @@
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+	const activeStyle = "underline underline-offset-4 teal-200";
+
+	return (
+		<nav className="flex justify-between w-full items-center fixed z-10 py-5 px-8 text-sm font-light">
+			<ul className="flex items-center gap-3 ">
+				<li className="font-bold text-lg">
+					<NavLink to="/">Shopi</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						All
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/clothes"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						Clothes
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/electronics"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						Electronics
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/fornitures"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						Fornitures
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/toys"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						Toys
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/others"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						Others
+					</NavLink>
+				</li>
+			</ul>
+			<ul className="flex items-center gap-3">
+				<li className="text-black/60">hello@reactecommerce.com</li>
+				<li>
+					<NavLink
+						to="/my-orders"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						My Orders
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/my-account"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						My Account
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to="/sign-in"
+						className={({ isActive }) => (isActive ? activeStyle : undefined)}
+					>
+						Sign In
+					</NavLink>
+				</li>
+				<li>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="1em"
+						viewBox="0 0 576 512"
+					>
+						<path d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z" />
+					</svg>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export { Navbar };
