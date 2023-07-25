f9408accdf309205fc483f647e372266b6461af6
Mateo <mateobetancurb@gmail.com>
2023-07-25 14:17:32 -0500

    rendering data from api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,22 @@
-const Card = () => {
+const Card = (data) => {
 	return (
 		<div className="bg-gray-200 cursor-pointer w-56 h-60 rounded-lg ">
 			<figure className="relative mb-2 w-full h-4/5">
 				<span className="absolute bottom-0 left-0 m-2 px-2 bg-white/70 rounded-full text-black text-xs">
-					Electronics
+					{data?.data?.category}
 				</span>
 				<img
 					className="w-full h-full object-cover rounded-lg"
-					src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-					alt="headphones"
+					src={data?.data?.image}
+					alt={data?.data?.title}
 				/>
 				<button className="absolute top-0 right-0 m-2 flex justify-center items-center bg-white w-6 h-6 rounded-full">
 					+
 				</button>
 			</figure>
 			<p className="flex justify-between px-3">
-				<span className="text-sm font-light">Headphones</span>
-				<span className="text-lg font-bold">$300</span>
+				<span className="text-sm font-light truncate">{data?.data?.title}</span>
+				<span className="text-lg font-bold">${data?.data?.price}</span>
 			</p>
 		</div>
 	);
