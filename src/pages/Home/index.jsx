a171e5c7462e1193b3e8725b3fbd9c31ffbc637d
Mateo <mateobetancurb@gmail.com>
2023-07-25 14:17:54 -0500

    get api data and send to card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react";
 import { Layout } from "../../components/Layout";
 import { Card } from "../../components/Card";
 
 const Home = () => {
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const getApiData = async () => {
+			const url = "https://fakestoreapi.com/products";
+			const response = await fetch(url);
+			const data = await response.json();
+			setItems(data);
+		};
+		getApiData();
+	}, []);
+
 	return (
 		<Layout>
-			Home
-			<Card />
+			<div className="grid gap-5 grid-cols-4 w-full max-w-screen-lg">
+				{items?.map((item) => (
+					<Card key={item.id} data={item} />
+				))}
+			</div>
 		</Layout>
 	);
 };
