af17443a451854808430ccf44bab841aafed735f
Mateo <mateobetancurb@gmail.com>
2023-07-26 15:03:56 -0500

    icon added and connect component with context

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,12 @@
 import { useEcommerce } from "../hooks/useEcommerce";
 
 const Card = (data) => {
-	const { incrementShoppingCart } = useEcommerce();
+	const { incrementShoppingCart, openProductDetail } = useEcommerce();
 	return (
-		<div className="bg-gray-200 cursor-pointer w-56 h-60 rounded-lg ">
+		<div
+			onClick={openProductDetail}
+			className="bg-gray-200 cursor-pointer w-56 h-60 rounded-lg hover:bg "
+		>
 			<figure className="relative mb-2 w-full h-4/5">
 				<span className="absolute bottom-0 left-0 m-2 px-2 bg-white/70 rounded-full text-black text-xs">
 					{data?.data?.category}
@@ -17,7 +20,20 @@ const Card = (data) => {
 					className="absolute top-0 right-0 m-2 flex justify-center items-center bg-white w-6 h-6 rounded-full hover:bg-black color-black hover:text-white transition-colors duration-200"
 					onClick={incrementShoppingCart}
 				>
-					+
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M12 4.5v15m7.5-7.5h-15"
+						/>
+					</svg>
 				</button>
 			</figure>
 			<p className="flex justify-between px-3">
