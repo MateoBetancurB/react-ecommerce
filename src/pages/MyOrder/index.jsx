3c5fcc9172fe447c7f893dc4262eee20773ae9c0
Mateo <mateobetancurb@gmail.com>
2023-08-03 12:49:52 -0500

    show user order

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,7 +1,26 @@
 import { Layout } from "../../components/Layout";
+import { useEcommerce } from "../../hooks/useEcommerce";
+import { OrderCard } from "../../components/OrderCard";
 
 const MyOrder = () => {
-	return <Layout>My order</Layout>;
+	const { order } = useEcommerce();
+
+	return (
+		<Layout>
+			<h2 className="pt-10 pb-5 font-extrabold">My Order</h2>
+			<div>
+				{order?.slice(-1)[0].products.map((product) => (
+					<OrderCard
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						image={product.image}
+						price={product.price}
+					/>
+				))}
+			</div>
+		</Layout>
+	);
 };
 
 export { MyOrder };
