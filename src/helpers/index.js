633022eca18bdfc806d65357e2ebcf4a05a64543
Mateo <mateobetancurb@gmail.com>
2023-08-03 14:05:03 -0500

    helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,4 +3,20 @@ const truncateTitle = (title, numWords) => {
 	return words.slice(0, numWords).join(" ");
 };
 
-export { truncateTitle };
+const randomId = () => {
+	const random = Math.random().toString(36).substring(2);
+	const date = Date.now().toString(36);
+	return random + date;
+};
+
+const formatDate = (date) => {
+	const newDate = new Date(date);
+	const options = {
+		year: "numeric",
+		month: "short",
+		day: "2-digit",
+	};
+	return newDate.toLocaleDateString("es-ES", options);
+};
+
+export { truncateTitle, randomId, formatDate };
