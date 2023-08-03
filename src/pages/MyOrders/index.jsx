9bfd835d4603bf4a00974aaa0db3a4da01b1d1e2
Mateo <mateobetancurb@gmail.com>
2023-08-03 14:05:51 -0500

    show all orders user

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
+import { useEcommerce } from "../../hooks/useEcommerce";
 import { Layout } from "../../components/Layout";
+import { OrdersCard } from "../../components/OrdersCard";
 
 const MyOrders = () => {
-	return <Layout>My orders</Layout>;
+	const { order } = useEcommerce();
+
+	return (
+		<Layout>
+			<div className="flex items-center justify-center gap-5 pt-10 pb-5 w-80 ">
+				<h1>My orders</h1>
+			</div>
+			{order.map((order, index) => (
+				<Link key={index} to={`/my-orders/${order.id}`}>
+					<OrdersCard
+						totalPrice={order.totalPrice}
+						totalProducts={order.totalProducts}
+					/>
+				</Link>
+			))}
+		</Layout>
+	);
 };
 
 export { MyOrders };
