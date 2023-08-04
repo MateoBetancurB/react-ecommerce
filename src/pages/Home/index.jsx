808d8488337f9a7c7c459487327c79944da835bc
Mateo <mateobetancurb@gmail.com>
2023-08-04 14:23:35 -0500

    render view filters with input search

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,14 +4,37 @@ import { Card } from "../../components/Card";
 import { ProductDetail } from "../../components/ProductDetail";
 
 const Home = () => {
-	const { items } = useEcommerce();
+	const { items, inputSearch, setInputSearch, filteredItems } = useEcommerce();
+
+	const handleChange = (e) => {
+		setInputSearch(e.target.value);
+	};
+
+	const renderView = () => {
+		if (inputSearch.length > 0) {
+			if (filteredItems.length > 0) {
+				return filteredItems.map((item) => <Card key={item.id} data={item} />);
+			} else {
+				return (
+					<p className="col-span-4 text-center font-bold">No results found</p>
+				);
+			}
+		} else {
+			return items.map((item) => <Card key={item.id} data={item} />);
+		}
+	};
 
 	return (
 		<Layout>
+			<input
+				onChange={handleChange}
+				value={inputSearch}
+				type="text"
+				placeholder="Search a product..."
+				className="rounded-lg border border-black w-80 px-4 py-2 mt-4 mb-7"
+			/>
 			<div className="grid gap-5 sm:grid-cols-2 md:grid-cols-4 w-full max-w-screen-lg">
-				{items?.map((item) => (
-					<Card key={item.id} data={item} />
-				))}
+				{renderView()}
 			</div>
 			<ProductDetail />
 		</Layout>
