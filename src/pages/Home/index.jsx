0791be2ca7e9e59bf4117ed393a2885a0cb0d4fa
Mateo <mateobetancurb@gmail.com>
2023-08-04 21:36:17 -0500

    conditional rendering - isLoading

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,20 +4,11 @@ import { Card } from "../../components/Card";
 import { ProductDetail } from "../../components/ProductDetail";
 
 const Home = () => {
-	const { searchByTitle, setSearchByTitle, filteredItems } = useEcommerce();
+	const { isLoading, searchByTitle, setSearchByTitle, filteredItems } =
+		useEcommerce();
 
 	const handleChange = (e) => {
 		setSearchByTitle(e.target.value);
-	};
-
-	const renderView = () => {
-		if (filteredItems.length > 0) {
-			return filteredItems.map((item) => <Card key={item.id} data={item} />);
-		} else {
-			return (
-				<p className="col-span-4 text-center font-bold">No results found</p>
-			);
-		}
 	};
 
 	return (
@@ -29,9 +20,19 @@ const Home = () => {
 				placeholder="Search a product..."
 				className="rounded-lg border border-black w-80 px-4 py-2 mt-4 mb-7"
 			/>
-			<div className="grid gap-5 sm:grid-cols-2 md:grid-cols-4 w-full max-w-screen-lg">
-				{renderView()}
-			</div>
+			{isLoading ? (
+				<p className="font-bold text-lg">
+					Products are loading, wait a moment...
+				</p>
+			) : (
+				<div className="grid gap-5 sm:grid-cols-2 md:grid-cols-4 w-full max-w-screen-lg">
+					{filteredItems.length > 0 ? (
+						filteredItems.map((item) => <Card key={item.id} data={item} />)
+					) : (
+						<p className="col-span-4 text-center font-bold">No results found</p>
+					)}
+				</div>
+			)}
 			<ProductDetail />
 		</Layout>
 	);
